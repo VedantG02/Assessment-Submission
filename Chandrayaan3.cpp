43c7eb71c33b833cd1c97f07ef7ece3f525c8980
VedantG02 <94516696+VedantG02@users.noreply.github.com>
2023-09-15 14:37:19 +0530

    Add files via upload

--- Chandrayaan3.cpp
+++ Chandrayaan3.cpp
@@ -4,175 +4,201 @@ using namespace std;
 
 class Chandrayaan3 {
 
-    int positionX, positionY, positionZ;
-    char orientation;
+    int posX, posY, posZ;
+    char heading;
 
 public:
     Chandrayaan3() {
-        positionX = positionY = positionZ = 0;
-        orientation = 'N';
+        posX = posY = posZ = 0;
+        heading = 'O';
     }
 
     Chandrayaan3(int x, int y, int z, char dir) {
-        positionX = x;
-        positionY = y;
-        positionZ = z;
-        orientation = dir;
+        posX = x;
+        posY = y;
+        posZ = z;
+        heading = dir;
     }
 
     int getX() {
-        return positionX;
+        return posX;
     }
 
     int getY() {
-        return positionY;
+        return posY;
     }
 
     int getZ() {
-        return positionZ;
+        return posZ;
     }
 
-    char getOrientation() {
-        return orientation;
+    char getHeading() {
+        return heading;
     }
 
     void addToX(int a) {
-        positionX += a;
+        posX += a;
     }
 
     void addToY(int a) {
-        positionY += a;
+        posY += a;
     }
 
     void addToZ(int a) {
-        positionZ += a;
+        posZ += a;
     }
 
-    void setOrientation(char dir) {
-        orientation = dir;
+    void setHeading(char a) {
+        heading = a;
     }
 
     void printFinalStatus() {
-        cout << "Final Coordinates: (" << positionX << "," << positionY << "," << positionZ << ")" << endl;
-        cout << "Final Orientation: " << orientation << endl;
+        cout << "Final Coordinates : (" << posX << "," << posY << "," << posZ << ")" << endl;
+        cout << "Final Heading : " << heading << endl;
     }
 };
 
 int main() {
 
     cout << "Enter initial coordinates (x, y, z)" << endl;
-    int startX, startY, startZ;
-    cout << "Enter x coordinate: ";
-    cin >> startX;
-    cout << "Enter y coordinate: ";
-    cin >> startY;
-    cout << "Enter z coordinate: ";
-    cin >> startZ;
+    int initialX, initialY, initialZ;
+    cout << "Enter x coordinate : ";
+    cin >> initialX;
+    cout << "Enter y coordinate : ";
+    cin >> initialY;
+    cout << "Enter z coordinate : ";
+    cin >> initialZ;
 
-    char initialOrientation, tempOrientation;
-    cout << "Enter initial orientation (N, E, S, W, U, D): ";
-    cin >> initialOrientation;
+    char initialHeading, tempHeading;
+    cout << "Enter initial heading : ";
+    cin >> initialHeading;
 
-    Chandrayaan3 rover(startX, startY, startZ, initialOrientation);
+    Chandrayaan3 rover(initialX, initialY, initialZ, initialHeading);
 
-    cout << "Enter commands and enter '0' after inputting all commands to terminate input:" << endl;
+    cout << "Enter commands and enter 0 after inputting all commands to terminate input : " << endl;
 
     vector<char> commands;
 
-    while (true) {
-        char command;
-        cin >> command;
-        if (command == '0') {
+    while (1) {
+        char cmd;
+        cin >> cmd;
+        if (cmd == '0') {
             break;
-        } else {
-            commands.push_back(command);
+        }
+        else {
+            commands.push_back(cmd);
         }
     }
 
     for (char cmd : commands) {
+
         switch (cmd) {
-            case 'u':
-                rover.setOrientation('U');
-                break;
+        case 'u':
+            rover.setHeading('U');
+            break;
 
-            case 'd':
-                rover.setOrientation('D');
-                break;
+        case 'd':
+            rover.setHeading('D');
+            break;
 
-            case 'l':
-                tempOrientation = rover.getOrientation();
-                if (tempOrientation == 'N') {
-                    rover.setOrientation('W');
-                } else if (tempOrientation == 'E') {
-                    rover.setOrientation('N');
-                } else if (tempOrientation == 'S') {
-                    rover.setOrientation('E');
-                } else if (tempOrientation == 'W') {
-                    rover.setOrientation('S');
-                } else if (tempOrientation == 'U') {
-                    rover.setOrientation('N');
-                } else if (tempOrientation == 'D') {
-                    rover.setOrientation('S');
-                }
-                break;
+        case 'l':
+            tempHeading = rover.getHeading();
+            if (tempHeading == 'N') {
+                rover.setHeading('W');
+            }
+            else if (tempHeading == 'E') {
+                rover.setHeading('N');
+            }
+            else if (tempHeading == 'S') {
+                rover.setHeading('E');
+            }
+            else if (tempHeading == 'W') {
+                rover.setHeading('S');
+            }
+            else if (tempHeading == 'U') {
+                rover.setHeading('N');
+            }
+            else if (tempHeading == 'D') {
+                rover.setHeading('S');
+            }
+            break;
 
-            case 'r':
-                tempOrientation = rover.getOrientation();
-                if (tempOrientation == 'N') {
-                    rover.setOrientation('E');
-                } else if (tempOrientation == 'E') {
-                    rover.setOrientation('S');
-                } else if (tempOrientation == 'S') {
-                    rover.setOrientation('W');
-                } else if (tempOrientation == 'W') {
-                    rover.setOrientation('N');
-                } else if (tempOrientation == 'U') {
-                    rover.setOrientation('S');
-                } else if (tempOrientation == 'D') {
-                    rover.setOrientation('N');
-                }
-                break;
+        case 'r':
 
-            case 'f':
-                tempOrientation = rover.getOrientation();
-                if (tempOrientation == 'N') {
-                    rover.addToY(1);
-                } else if (tempOrientation == 'E') {
-                    rover.addToX(1);
-                } else if (tempOrientation == 'S') {
-                    rover.addToY(-1);
-                } else if (tempOrientation == 'W') {
-                    rover.addToX(-1);
-                } else if (tempOrientation == 'U') {
-                    rover.addToZ(1);
-                } else if (tempOrientation == 'D') {
-                    rover.addToZ(-1);
-                }
-                break;
+            tempHeading = rover.getHeading();
+            if (tempHeading == 'N') {
+                rover.setHeading('E');
+            }
+            else if (tempHeading == 'E') {
+                rover.setHeading('S');
+            }
+            else if (tempHeading == 'S') {
+                rover.setHeading('W');
+            }
+            else if (tempHeading == 'W') {
+                rover.setHeading('N');
+            }
+            else if (tempHeading == 'U') {
+                rover.setHeading('S');
+            }
+            else if (tempHeading == 'D') {
+                rover.setHeading('N');
+            }
+            break;
 
-            case 'b':
-                tempOrientation = rover.getOrientation();
-                if (tempOrientation == 'N') {
-                    rover.addToY(-1);
-                } else if (tempOrientation == 'E') {
-                    rover.addToX(-1);
-                } else if (tempOrientation == 'S') {
-                    rover.addToY(1);
-                } else if (tempOrientation == 'W') {
-                    rover.addToX(1);
-                } else if (tempOrientation == 'U') {
-                    rover.addToZ(-1);
-                } else if (tempOrientation == 'D') {
-                    rover.addToZ(1);
-                }
-                break;
+        case 'f':
 
-            default:
-                break;
+            tempHeading = rover.getHeading();
+
+            if (tempHeading == 'N') {
+                rover.addToY(1);
+            }
+            else if (tempHeading == 'E') {
+                rover.addToX(1);
+            }
+            else if (tempHeading == 'S') {
+                rover.addToY(-1);
+            }
+            else if (tempHeading == 'W') {
+                rover.addToX(-1);
+            }
+            else if (tempHeading == 'U') {
+                rover.addToZ(1);
+            }
+            else if (tempHeading == 'D') {
+                rover.addToZ(-1);
+            }
+
+            break;
+
+        case 'b':
+            tempHeading = rover.getHeading();
+
+            if (tempHeading == 'N') {
+                rover.addToY(-1);
+            }
+            else if (tempHeading == 'E') {
+                rover.addToX(-1);
+            }
+            else if (tempHeading == 'S') {
+                rover.addToY(1);
+            }
+            else if (tempHeading == 'W') {
+                rover.addToX(1);
+            }
+            else if (tempHeading == 'U') {
+                rover.addToZ(-1);
+            }
+            else if (tempHeading == 'D') {
+                rover.addToZ(1);
+            }
+
+        default:
+            break;
         }
     }
 
-    cout << "\nFinal Configuration of Chandrayaan-3:\n" << endl;
+    cout << "\nFinal Configuration of Chandrayaan3\n" << endl;
 
     rover.printFinalStatus();
 }
